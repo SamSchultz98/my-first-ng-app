fd4f907ee79489171e9f2175026afba24333ae8a
Sam Schultz <sam.schultz@outlook.com>
2022-11-01 00:21:14 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//Below is where you make your properties to be binded in the html
 export class AppComponent {
-  title = 'my-first-ng-app';
+  title = 'My first Angular App';
+  name: string = "Sam";
 }
